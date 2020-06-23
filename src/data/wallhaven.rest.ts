230ce1f91467b1b26111f83a876c5c79ac38787d
Carlos Jaramillo <cejl397@gmail.com>
2020-06-23 11:47:41 +0200

    feat: add default vertical ratios

--- src/data/wallhaven.rest.ts
+++ src/data/wallhaven.rest.ts
@@ -14,6 +14,7 @@ export class WallhavenApi extends RESTDataSource {
     const data = await this.get("search", {
       q: searchTerm,
       page,
+      ratios: ["10x16", "9x16"],
     });
 
     return {
